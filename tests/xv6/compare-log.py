b62673b942cc8d93d43a435707ae01f3094e7283
ainozaki <aiai10piano@gmail.com>
2023-01-24 08:49:07 +0900

    update test tool

--- tests/xv6/compare-log.py
+++ tests/xv6/compare-log.py
@@ -1,116 +1,101 @@
 import sys
 
-def compare_files(gdb_path, emu_path):
-    f_gdb = open(gdb_path)
-    f_emu = open(emu_path)
+class Log:
+    def __init__(self, gdb_file, emu_file):
+        self.f_gdb = open(gdb_file)
+        self.f_emu = open(emu_file)
+        self.loglist_gdb = [s.strip() for s in self.f_gdb.readlines()]
+        self.loglist_emu = [s.strip() for s in self.f_emu.readlines()]
+        self.len_gdb = len(self.loglist_gdb)
+        self.len_emu = len(self.loglist_emu)
+        self.idx_gdb = 0
+        self.idx_emu = 0
+        self.idx_inst = 0
+        self.num_insts = int(self.loglist_emu[0], 10)
+        self.pc = 0
 
-    loglist_gdb = [s.strip() for s in f_gdb.readlines()]
-    loglist_emu = [s.strip() for s in f_emu.readlines()]
-    len_gdb = len(loglist_gdb)
-    len_emu = len(loglist_emu)
-    idx_gdb = 0
-    idx_emu = 0
-    inst_idx = 0
-
-    # Number of insts
-    num_insts = int(loglist_emu[0], 10)
-
-    while (idx_gdb < len_gdb) and (idx_emu < len_emu):
-        tmp1 = loglist_gdb[idx_gdb].split()
-        idx_gdb += 1
-        tmp2 = loglist_emu[idx_emu].split()
-        idx_emu += 1
+    def compare_item(self, name):
+        tmp1 = self.loglist_gdb[self.idx_gdb].split()
+        tmp2 = self.loglist_emu[self.idx_emu].split()
+        self.idx_gdb += 1
+        self.idx_emu += 1
+        assert(tmp1[0] == name)
+        assert(tmp2[0] == name)
         
-        # sync
-        while tmp1[0] != "===":
-            tmp1 = loglist_gdb[idx_gdb].split()
-            idx_gdb += 1
-        while tmp2[0] != "===":
-            tmp2 = loglist_emu[idx_emu].split()
-            idx_emu += 1
-        
-        # index
-        assert(tmp1[1] == tmp2[1])
-
-        # pc
-        tmp1 = loglist_gdb[idx_gdb].split()
-        idx_gdb += 1
-        tmp2 = loglist_emu[idx_emu].split()        
-        idx_emu += 1
-        assert(tmp1[0] == "pc")
-        assert(tmp2[0] == "pc")
+        if (name == "sp"):
+            return
+        if (name == "pc"):
+            self.pc = int(tmp1[1], 16)
         if tmp1[1] != tmp2[1]:
-            print("pc ng", format(int(tmp1[1], 16), "x"), "vs", format(int(tmp2[1], 16), "x"))
-            return 1
-        pc = int(tmp1[1], 16)
+            print("[{}][{}] gdb:{} vs emu:{}".format(self.idx_inst, name, format(int(tmp1[1], 16), "x"), format(int(tmp2[1], 16), "x")))
+            exit()
         
-        # sp
-        tmp1 = loglist_gdb[idx_gdb].split()
-        idx_gdb += 1
-        tmp2 = loglist_emu[idx_emu].split()        
-        idx_emu += 1
-        
-        # cpsr
-        tmp1 = loglist_gdb[idx_gdb].split()
-        idx_gdb += 1
-        #tmp2 = loglist_emu[idx_emu].split()        
-        #idx_emu += 1
-        
-        # w0
-        tmp1 = loglist_gdb[idx_gdb].split()
-        idx_gdb += 1
-        tmp2 = loglist_emu[idx_emu].split()        
-        idx_emu += 1
-        assert(tmp1[0] == "w0")
-        assert(tmp2[0] == "w0")
-        if tmp1[1] != tmp2[1]:
-            print("failed: w0", format(int(tmp1[1], 16), "x"), "vs", format(int(tmp2[1], 16), "x"))
-            return 1
-        
-        # w1
-        tmp1 = loglist_gdb[idx_gdb].split()
-        idx_gdb += 1
-        tmp2 = loglist_emu[idx_emu].split()        
-        idx_emu += 1
-        assert(tmp1[0] == "w1")
-        assert(tmp2[0] == "w1")
-        if tmp1[1] != tmp2[1]:
-            print("w1 ng", format(int(tmp1[1], 16), "x"), "vs", format(int(tmp2[1], 16), "x"))
-            return 1
-        
-        # w2
-        tmp1 = loglist_gdb[idx_gdb].split()
-        idx_gdb += 1
-        tmp2 = loglist_emu[idx_emu].split()        
-        idx_emu += 1
-        assert(tmp1[0] == "w2")
-        assert(tmp2[0] == "w2")
-        if tmp1[1] != tmp2[1]:
-            print("w2 ng", format(int(tmp1[1], 16), "x"), "vs", format(int(tmp2[1], 16), "x"))
-            return 1
-       
-        # disas
-        tmp2 = loglist_emu[idx_emu]        
-        idx_emu += 1
-        print("[", inst_idx, "]", format(pc, "x"), tmp2)
 
-        # loop up to `num_insts`
-        inst_idx += 1
-        if inst_idx == num_insts:
-            break
+    def compare_files(self):
+        while (self.idx_gdb < self.len_gdb) and (self.idx_emu < self.len_emu):
+            tmp1 = self.loglist_gdb[self.idx_gdb].split()
+            tmp2 = self.loglist_emu[self.idx_emu].split()
+            self.idx_gdb += 1
+            self.idx_emu += 1
+            
+            # sync
+            while tmp1[0] != "===":
+                tmp1 = self.loglist_gdb[self.idx_gdb].split()
+                self.idx_gdb += 1
+            while tmp2[0] != "===":
+                tmp2 = self.loglist_emu[self.idx_emu].split()
+                self.idx_emu += 1
+            
+            # index
+            assert(tmp1[1] == tmp2[1])
 
-    f_gdb.close()
-    f_emu.close()
+            # pc
+            self.compare_item("pc")
+            
+            # sp
+            self.compare_item("sp")
+            
+            # cpsr
+            tmp1 = self.loglist_gdb[self.idx_gdb].split()
+            self.idx_gdb += 1
+            #tmp2 = loglist_emu[idx_emu].split()        
+            #idx_emu += 1
+            
+            # general registers
+            self.compare_item("x0")
+            self.compare_item("x1")
+            self.compare_item("x2")
+            self.compare_item("x3")
+            self.compare_item("x19")
+            self.compare_item("x29")
+            self.compare_item("x30")
+            
+            # disas
+            tmp2 = self.loglist_emu[self.idx_emu]        
+            self.idx_emu += 1
+            print("[{}] {} {}".format(self.idx_inst, format(self.pc, "x"), tmp2))
+
+            # loop up to `num_insts`
+            self.idx_inst += 1
+            if self.idx_inst == self.num_insts:
+                break
+
+        self.f_gdb.close()
+        self.f_emu.close()
     
 def main():
+    """
     argv = sys.argv
     if len(argv) < 3:
         print("usage:", argv[0], "<gdb-file> <emu-file>")
         return
     gdb_path = argv[1]
     emu_path = argv[2]
-    print("compare", gdb_path, "and", emu_path)
-    compare_files(gdb_path, emu_path)
+    """
+    gdb_path = "gdb-log.txt"
+    emu_path = "emu-log.txt"
+    log = Log(gdb_path, emu_path)
+    log.compare_files()
 
 if __name__ == "__main__":
     main()
