5a922b9662930dbd7a2f2f7129029017eedc3b2c
ainozaki <aiai10piano@gmail.com>
2023-01-24 10:47:43 +0900

    update test script to support ldxr

--- tests/xv6/compare-log.py
+++ tests/xv6/compare-log.py
@@ -13,12 +13,23 @@ class Log:
         self.idx_inst = 0
         self.num_insts = int(self.loglist_emu[0], 10)
         self.pc = 0
+        self.registers = ["pc", "sp", "x0", "x1", "x2", "x3", "x19", "x29", "x30"]
+        self.idx_delta = 0
+    
+    def read_gdb_oneline(self):
+        line = self.loglist_gdb[self.idx_gdb].split()
+        self.idx_gdb += 1
+        return line
+
+    def read_emu_oneline(self):
+        line = self.loglist_emu[self.idx_emu].split()
+        self.idx_emu += 1
+        return line
 
     def compare_item(self, name):
-        tmp1 = self.loglist_gdb[self.idx_gdb].split()
-        tmp2 = self.loglist_emu[self.idx_emu].split()
-        self.idx_gdb += 1
-        self.idx_emu += 1
+        tmp1 = self.read_gdb_oneline()
+        tmp2 = self.read_emu_oneline()
+        
         assert(tmp1[0] == name)
         assert(tmp2[0] == name)
         
@@ -30,24 +41,32 @@ class Log:
             print("[{}][{}] gdb:{} vs emu:{}".format(self.idx_inst, name, format(int(tmp1[1], 16), "x"), format(int(tmp2[1], 16), "x")))
             exit()
         
+    def read_until(self, name):
+        while (1):
+            self.idx_delta += 1
+            tmp = self.read_emu_oneline()
+            assert(tmp[0] == "===")
+            for i in range(len(self.registers)):
+                tmp = self.read_emu_oneline()
+                assert(tmp[0] == self.registers[i])
+            tmp = self.read_emu_oneline()
+            if (tmp[0] == name):
+                break
+            
 
     def compare_files(self):
         while (self.idx_gdb < self.len_gdb) and (self.idx_emu < self.len_emu):
-            tmp1 = self.loglist_gdb[self.idx_gdb].split()
-            tmp2 = self.loglist_emu[self.idx_emu].split()
-            self.idx_gdb += 1
-            self.idx_emu += 1
+            tmp1 = self.read_gdb_oneline()
+            tmp2 = self.read_emu_oneline()
             
             # sync
             while tmp1[0] != "===":
-                tmp1 = self.loglist_gdb[self.idx_gdb].split()
-                self.idx_gdb += 1
+                tmp1 = self.read_gdb_oneline()
             while tmp2[0] != "===":
-                tmp2 = self.loglist_emu[self.idx_emu].split()
-                self.idx_emu += 1
+                tmp2 = self.read_emu_oneline()
             
             # index
-            assert(tmp1[1] == tmp2[1])
+            assert(int(tmp1[1]) == int(tmp2[1]) - self.idx_delta)
 
             # pc
             self.compare_item("pc")
@@ -56,10 +75,7 @@ class Log:
             self.compare_item("sp")
             
             # cpsr
-            tmp1 = self.loglist_gdb[self.idx_gdb].split()
-            self.idx_gdb += 1
-            #tmp2 = loglist_emu[idx_emu].split()        
-            #idx_emu += 1
+            tmp1 = self.read_gdb_oneline()
             
             # general registers
             self.compare_item("x0")
@@ -71,9 +87,13 @@ class Log:
             self.compare_item("x30")
             
             # disas
-            tmp2 = self.loglist_emu[self.idx_emu]        
+            tmp2 = self.loglist_emu[self.idx_emu]
             self.idx_emu += 1
             print("[{}] {} {}".format(self.idx_inst, format(self.pc, "x"), tmp2))
+            
+            # adjust
+            if (tmp2.split()[0] == "ldxr"):
+                self.read_until("stxr")
 
             # loop up to `num_insts`
             self.idx_inst += 1
